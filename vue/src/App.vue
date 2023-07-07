222b8fea8ab267d24d7221b26d2f69358868eabe
David Dume <dumedavid11@gmail.com>
2023-07-07 17:26:58 +0200

    chiamata api per generazione key

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,8 +3,8 @@
         <div class="container text-center bg-white">
             <h2 class="m-5 text-3xl font-medium">Url Shortener</h2>
             <div>
-                <input type="text" class="border px-5 py-2 rounded-lg focus-ring" />
-                <button class="text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-2.5">
+                <input type="text" class="border px-2 py-2 rounded-lg focus-ring" v-model="longUrl" />
+                <button class="text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-2.5" @click="generateLink">
                     Shorten
                 </button>
             </div>
@@ -13,8 +13,27 @@
 </template>
 
 <script>
+    import {ref} from 'vue'
+    import {store} from './store'
+    import axios from 'axios'
 
-    
+    export default {
+        setup() {
+            const longUrl = ref('');
+
+            const generateLink = () => {
+                axios.post(`${store.baseUrl}/generate-key`, {longUrl: longUrl.value}).then(res => {
+                    console.log(res.data);
+                });
+                longUrl.value = ''
+            }
+
+            return {
+                longUrl,
+                generateLink
+            }
+        }
+    }
 
 </script>
 
