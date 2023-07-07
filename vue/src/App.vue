41ba502d192653d84df37f682d2fcaba02216dc9
David Dume <dumedavid11@gmail.com>
2023-07-07 17:18:40 +0200

    creazione mockup grafico

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,11 +1,28 @@
+<template>
+    <div class="flex justify-center items-center h-screen">
+        <div class="container text-center bg-white">
+            <h2 class="m-5 text-3xl font-medium">Url Shortener</h2>
+            <div>
+                <input type="text" class="border px-5 py-2 rounded-lg focus-ring" />
+                <button class="text-white bg-blue-700 hover:bg-blue-800  focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 ml-2.5">
+                    Shorten
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
 <script>
+
+    
 
 </script>
 
-<template>
-  <div class="text-3xl underline">Hello World</div>
-</template>
-
-<style scoped>
-
+<style lang="scss">
+.container {
+    
+    max-width: 758px;
+    height: 200px;
+    box-shadow: 9px 19px 34px -13px rgba(0,0,0,0.1);
+}
 </style>
